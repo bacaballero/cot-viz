f206ea1535c419cbfbc41777dcff34e0007a1069
bacaballero <brentcaballero95@yahoo.com>
2022-05-22 14:16:38 -0500

    add dropdown menu to select which commodity to display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
 import * as jsonData from './json_data.json'
+import { useState, useEffect } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -23,45 +24,69 @@ ChartJS.register(
 );
 
 const data = jsonData
-console.log(data.data.map(n => n.name))
-
-const names = data.data.map(futures => futures.name)
-const dates = data.data.map(futures => futures.dates)
 
 function App() {
+  const [commodity, setCommodity] = useState('#2 HEATING OIL- NY HARBOR-ULSD - NEW YORK MERCANTILE EXCHANGE')
+  const [selectedCommodityData, setSelectedCommodityData] = useState([data.data.filter(future => future.name === commodity)])
+
+  useEffect(() => {
+    setSelectedCommodityData(data.data.filter(future => future.name === commodity))
+  }, [commodity])
+
+  const handleChange = (event) => {
+    setCommodity(event.target.value) 
+  }
+
+  console.log(commodity)
+
   return (
     <div>
+      <label>
+        Select a commodity 
+        <select value={commodity} onChange={handleChange}>
+          {data.data.map(future =>
+            <option key={future.name} value={future.name}>{future.name}</option>)}
+        </select>
+      </label>
       <h1>Commitment of Traders Data</h1>
-      {data.data.map(future => 
-        <div key={future.name}>
-          <Line options={{responsive: true, plugins: {title: {display: true, text: future.name}}}} 
+        <div key={selectedCommodityData[0].name}>
+          <Line options={{responsive: true, layout: {padding: 80} , plugins: {title: {display: true, text: selectedCommodityData[0].name}}}} 
             data={
               {
-                labels: future.dates,
+                labels: selectedCommodityData[0].dates,
                 datasets: [
                   {
                     label: 'Commercial Hedgers',
-                    data: future.commercial,
+                    data: selectedCommodityData[0].commercial,
                     borderColor: 'green',
-                    backgroundColor: 'green'
+                    backgroundColor: 'green',
+                    borderJoinStyle: 'round',
+                    pointStyle: 'line',
+                    pointBorderWidth: 0,
+                    
                   },
                   {
                     label: 'Small Traders',
-                    data: future['non reporting'],
+                    data: selectedCommodityData[0]['non reporting'],
                     borderColor: 'blue',
-                    backgroundColor: 'blue'
+                    backgroundColor: 'blue',
+                    borderJoinStyle: 'round',
+                    pointStyle: 'line',
+                    pointBorderWidth: 0,
                   },
                   {
                     label: 'Large Traders',
-                    data: future['non commercial'],
+                    data: selectedCommodityData[0]['non commercial'],
                     borderColor: 'red',
-                    backgroundColor: 'red'
+                    backgroundColor: 'red',
+                    borderJoinStyle: 'round',
+                    pointStyle: 'line',
+                    pointBorderWidth: 0
                   },
                 ]
               }
             }/>
         </div>
-      )}
     </div>
   );
 }
